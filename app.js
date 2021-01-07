4e2a179a35c3c956ac0363a4cdae717a4d151eae
Steven <Mahalak.steve@outlook.com>
2021-01-06 23:31:06 -0500

    hamburger working now

--- app.js
+++ app.js
@@ -59,6 +59,26 @@ function init(){
 
         current= pageNumber;
     }
+
+    const hamburger = document.querySelector(".menu");
+    const hamburgerLines = document.querySelectorAll (".menu line");
+    const navOpen = document.querySelector(".nav-open");
+    const contact = document.querySelector(".contact");
+    const social = document.querySelector(".social");
+    const logo = document.querySelector(".logo");
+
+    const tl = new TimelineMax({paused: true, reversed:true });
+
+    tl.to(navOpen, 0.5, {y:0})
+    .fromTo(contact,0.5, {opacity: 0, y:10}, {opacity: 1, y:0}, "-=0.1")
+    .fromTo(social,0.5, {opacity: 0, y:10}, {opacity: 1, y:0}, "-=0.5")
+    .fromTo(logo,0.2, {color: "white"}, {color: "black" }, "-=1")
+    .fromTo(hamburgerLines, 0.2, {stroke: "white" }, {stroke: "black"}, "-=1");
+          
+
+    hamburger.addEventListener("click", () => {
+        tl.reversed() ? tl.play() : tl.reverse();
+    })
 }
 
 
