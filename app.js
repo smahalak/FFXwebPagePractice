ea4ec784fb6b197505675e266954295ba5828dd9
Steven <Mahalak.steve@outlook.com>
2021-01-06 21:10:50 -0500

    added js but need to fix

---
+++ app.js
@@ -0,0 +1,62 @@
+function init(){
+    const slides = document.querySelectorAll('.slide');
+    const pages = document.querySelectorAll(".page");
+    const backgrounds = [
+        `radial-gradient(#2B3760, #0B1023)`, 
+        `radial-gradient(#4E3022, #161616)`, 
+        `radial-gradient(#4E4342, #161616)`
+    ];
+    //tracker
+    let current = 0;
+
+    slides.forEach((slide,index) => {
+        slide.addEventListener('click', function(){
+            changeDots(this);
+            nextSlide(index)
+        });
+    });
+
+    function changeDots(dot) {
+        slides.forEach(slide => {
+            slide.classList.remove("active");
+        });
+        dot.classList.add("active");
+    }
+
+    function nextSlide(pageNumber) {
+        const nextPage = pages[pageNumber];
+        const currentPage = pages [current];
+        const nextLeft = nextPage.querySelector(".hero .model-left");
+        const nextRight = nextPage.querySelector(".hero .model-right");
+        const currentLeft = currentPage.querySelector(".hero .model-left");
+        const currentRight = currentPage.querySelector(".hero .model-right");
+        const nextText = nextPage.querySelector(".details");
+        const portofolio = document.querySelector(".portofolio");
+
+        const tl = new TimelineMax();
+
+    tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" })
+        .fromTo(currentRight, 0.3, { y: "10%" }, { y: "-100%" }, "-=0.2" )
+        .to(portofolio, 0.3, {backgroundImage: backgrounds[pageNumber]})
+        .fromTo(
+            currentPage,
+            0.3,
+            {opacity:1, pointerEvents: "all"}, 
+            {opacity:0, pointerEvents: "none"}    
+        )
+        .fromTo(
+            nextPage,
+            0.3,
+            {opacity:0, pointerEvents: "none"}, 
+            {opacity:1, pointerEvents: "all"}, 
+            "-=0.6"
+        )
+        .fromTo(nextLeft, 0.3, {y: "-100%"}, {y:"-10%"}, "-=0.6")
+        .fromTo(nextRight, 0.3, {y: "-100%"}, {y:"10%"}, "-=0.8");
+
+        current= pageNumber;
+    }
+}
+
+
+init();
