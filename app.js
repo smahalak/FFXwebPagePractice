9814431a33a4dbba0036d72fe3b862778c4ac2ef
Steven <Mahalak.steve@outlook.com>
2021-01-06 22:56:29 -0500

    fixed js issue

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function init(){
-    const slides = document.querySelectorAll('.slide');
+    const slides = document.querySelectorAll(".slide");
     const pages = document.querySelectorAll(".page");
     const backgrounds = [
         `radial-gradient(#2B3760, #0B1023)`, 
@@ -10,9 +10,9 @@ function init(){
     let current = 0;
 
     slides.forEach((slide,index) => {
-        slide.addEventListener('click', function(){
+        slide.addEventListener("click", function() {
             changeDots(this);
-            nextSlide(index)
+            nextSlide(index);
         });
     });
 
@@ -37,26 +37,30 @@ function init(){
 
     tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" })
         .fromTo(currentRight, 0.3, { y: "10%" }, { y: "-100%" }, "-=0.2" )
-        .to(portofolio, 0.3, {backgroundImage: backgrounds[pageNumber]})
+        .to(portofolio, 0.3, {backgroundImage: backgrounds[pageNumber] })
         .fromTo(
             currentPage,
             0.3,
-            {opacity:1, pointerEvents: "all"}, 
-            {opacity:0, pointerEvents: "none"}    
+            { opacity: 1, pointerEvents: "all" }, 
+            { opacity: 0, pointerEvents: "none" }    
         )
         .fromTo(
             nextPage,
             0.3,
-            {opacity:0, pointerEvents: "none"}, 
-            {opacity:1, pointerEvents: "all"}, 
+            { opacity: 0, pointerEvents: "none" }, 
+            { opacity: 1, pointerEvents: "all" }, 
             "-=0.6"
         )
-        .fromTo(nextLeft, 0.3, {y: "-100%"}, {y:"-10%"}, "-=0.6")
-        .fromTo(nextRight, 0.3, {y: "-100%"}, {y:"10%"}, "-=0.8");
+        .fromTo(nextLeft, 0.3, { y: "-100%" }, { y:"-10%" }, "-=0.6")
+        .fromTo(nextRight, 0.3, { y: "-100%" }, { y:"10%" }, "-=0.8")
+        .fromTo(nextText, 0.3, { opacity: 0, y: 0}, {opacity: 1, y: 0})
+        .set(nextLeft, { clearProps: "all" })
+        .set(nextRight, { clearProps: "all" });
 
         current= pageNumber;
     }
 }
+
 
 
 init();
